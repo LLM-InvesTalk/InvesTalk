468660c122a3effb4088410861a9c97de0cf1c0e
Sang Min Park <minisstar@naver.com>
2024-09-07 22:24:11 +0900

    ✨ 분석 컴포넌트 추가

---
+++ investalk-front/src/components/detail/analyze/Analyze.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Analyze.css"; // CSS 파일 따로 관리
+
+const Analyze = () => {
+  return (
+    <div className="div-wrapper">
+      <div className="overlap-wrapper">
+        <div className="overlap-3">
+          <div className="text-wrapper">Assistock 분석서</div>
+          <div className="text-description">
+            Text Description...<br />
+            .................................................................<br />
+            .................................................................<br />
+            ...............................................................<br />
+            .........................................<br />
+            .............................................................
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Analyze;
