2a4d597a55888c5f479563b5db2b4baa62a45028
chaewon02~ <glgl246@naver.com>
2024-09-09 19:06:58 +0900

    ✨ add NavBar Component

---
+++ investalk-front/src/components/detail/layout/NavBar.js
@@ -0,0 +1,61 @@
+// import "./NavBar.css";
+
+import { useCallback, useState } from "react";
+import Input from "@mui/joy/Input";
+import { IconButton } from "@mui/joy";
+import SearchIcon from "@mui/icons-material/Search";
+
+const NavBar = () => {
+  const [keyword, setKeyword] = useState("");
+
+  const onChange = useCallback((e) => {
+    setKeyword(e.target.value);
+  }, []);
+
+  return (
+    <div className="frame-8">
+      <img
+        className="frame-9"
+        src="https://c.animaapp.com/8Gc7c0uK/img/frame-130.svg"
+        alt="Frame"
+      />
+      <Input
+        className="vector-wrapper"
+        placeholder="Search..."
+        value={keyword}
+        onChange={onChange}
+        startDecorator={
+          <IconButton
+            variant="plain"
+            size="sm"
+            onClick={() => setKeyword("")}
+            className="custom-icon-button"
+            sx={{
+              backgroundColor: "transparent",
+              "&:hover": {
+                backgroundColor: "transparent",
+                "--Icon-color": "currentColor",
+                color: "var(--joy-palette-neutral-plainHoverColor, #0b0d0e)",
+              },
+              "&:active": {
+                backgroundColor: "transparent",
+                "--Icon-color": "currentColor",
+              },
+            }}
+          >
+            <SearchIcon />
+          </IconButton>
+        }
+      />
+
+      <div className="navbar">
+        <div className="text-wrapper-9">Menu</div>
+        <div className="text-wrapper-10">News</div>
+        <div className="text-wrapper-9">Trend</div>
+        <div className="text-wrapper-9">Mypage</div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
