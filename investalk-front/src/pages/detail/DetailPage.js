071dd04d4f36a97f589fcca7c7552dd03d5d2f2f
chaewon02~ <glgl246@naver.com>
2024-09-09 16:47:52 +0900

    ✨ create detailPage and testPage

---
+++ investalk-front/src/pages/detail/DetailPage.js
@@ -0,0 +1,13 @@
+import SemiConductorCard from "../../components/SemiConductorCard";
+
+const DetailPage = () => {
+  return (
+    <div>
+      <div>
+        <SemiConductorCard></SemiConductorCard>
+      </div>
+    </div>
+  );
+};
+
+export default DetailPage;
---
+++ investalk-front/src/pages/test/index.js
@@ -0,0 +1,13 @@
+import StockInfoCard from "../../components/detail/stockinfo";
+
+const TestPage = () => {
+  return (
+    <div>
+      <div>
+        <StockInfoCard></StockInfoCard>
+      </div>
+    </div>
+  );
+};
+
+export default TestPage;
