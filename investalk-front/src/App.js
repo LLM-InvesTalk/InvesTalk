839583d35f4ba10f1305ecdfaaa7b71f403c5b37
chaewon02~ <glgl246@naver.com>
2024-09-16 17:31:21 +0900

    💄 app.js 전역 css 삭제

--- investalk-front/src/App.js
+++ investalk-front/src/App.js
@@ -1,10 +1,6 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
 
-// css
-import "./components/detail/css/DetailGlobals.css";
-import "./components/detail/css/DetailStyleguide.css";
-
 function App() {
   return (
     <div className="App">
