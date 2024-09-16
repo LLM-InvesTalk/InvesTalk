fb9711eea3dcfdbfa18ae125f9b279ca6c571b1e
chaewon02~ <glgl246@naver.com>
2024-09-16 17:32:17 +0900

    💄 NavBar css 모듈화

--- investalk-front/src/components/detail/nav/NavBar.js
+++ investalk-front/src/components/detail/nav/NavBar.js
@@ -3,6 +3,8 @@ import Input from "@mui/joy/Input";
 import { IconButton } from "@mui/joy";
 import SearchIcon from "@mui/icons-material/Search";
 
+import styles from "./NavBar.module.css";
+
 const NavBar = () => {
   const [keyword, setKeyword] = useState("");
 
@@ -11,23 +13,26 @@ const NavBar = () => {
   }, []);
 
   return (
-    <div className="frame-8">
+    <div className={styles["frame-8"]}>
       <img
-        className="frame-9"
+        className={styles["frame-9"]}
         src="https://c.animaapp.com/8Gc7c0uK/img/frame-130.svg"
         alt="Frame"
       />
       <Input
-        className="vector-wrapper"
+        className={styles["vector-wrapper"]}
         placeholder="Search..."
         value={keyword}
         onChange={onChange}
+        sx={{
+          borderRadius: "40px",
+        }}
         startDecorator={
           <IconButton
             variant="plain"
             size="sm"
             onClick={() => setKeyword("")}
-            className="custom-icon-button"
+            className={styles["custom-icon-button"]}
             sx={{
               backgroundColor: "transparent",
               "&:hover": {
@@ -46,11 +51,11 @@ const NavBar = () => {
         }
       />
 
-      <div className="navbar">
-        <div className="text-wrapper-9">Menu</div>
-        <div className="text-wrapper-10">News</div>
-        <div className="text-wrapper-9">Trend</div>
-        <div className="text-wrapper-9">Mypage</div>
+      <div className={styles["navbar"]}>
+        <div className={styles["text-wrapper-9"]}>Menu</div>
+        <div className={styles["text-wrapper-10"]}>News</div>
+        <div className={styles["text-wrapper-9"]}>Trend</div>
+        <div className={styles["text-wrapper-9"]}>Mypage</div>
       </div>
     </div>
   );
