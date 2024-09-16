986cb4106716186281eb55a362175a140613b50d
chaewon02~ <glgl246@naver.com>
2024-09-16 17:35:00 +0900

    💄 layout 컴포넌트 css 모듈화

--- investalk-front/src/pages/layout/index.js
+++ investalk-front/src/pages/layout/index.js
@@ -1,4 +1,4 @@
-import "./style.css";
+import styles from "./style.module.css";
 
 import { Outlet } from "react-router-dom";
 
@@ -7,57 +7,57 @@ import NavBar from "../../components/detail/nav/NavBar";
 const LayoutPage = () => {
   return (
     <div>
-      <div className="div-wrapper">
-        <div className="overlap">
-          <div className="group">
-            <div className="overlap-group">
+      <div className={styles["div-wrapper"]}>
+        <div className={styles["overlap"]}>
+          <div className={styles["group"]}>
+            <div className={styles["overlap-group"]}>
               <img
-                className="burst-star"
+                className={styles["burst-star"]}
                 src="https://c.animaapp.com/8Gc7c0uK/img/burst-star.svg"
                 alt="Burst Star"
               />
               <img
-                className="img"
+                className={styles["img"]}
                 src="https://c.animaapp.com/8Gc7c0uK/img/burst-star-3.svg"
                 alt="Burst Star 3"
               />
             </div>
-            <div className="div">
+            <div className={styles["div"]}>
               <img
-                className="burst-star-2"
+                className={styles["burst-star-2"]}
                 src="https://c.animaapp.com/8Gc7c0uK/img/burst-star-1.svg"
                 alt="Burst Star 1"
               />
               <img
-                className="burst-star-3"
+                className={styles["burst-star-3"]}
                 src="https://c.animaapp.com/8Gc7c0uK/img/burst-star-2.svg"
                 alt="Burst Star 2"
               />
             </div>
-            <div className="overlap-2">
+            <div className={styles["overlap-2"]}>
               <img
-                className="burst-pucker"
+                className={styles["burst-pucker"]}
                 src="https://c.animaapp.com/8Gc7c0uK/img/burst-pucker-2.svg"
                 alt="Burst Pucker"
               />
               <img
-                className="burst-pucker-2"
+                className={styles["burst-pucker-2"]}
                 src="https://c.animaapp.com/8Gc7c0uK/img/burst-pucker-2-1.svg"
                 alt="Burst Pucker 2"
               />
             </div>
             <img
-              className="looper"
+              className={styles["looper"]}
               src="https://c.animaapp.com/8Gc7c0uK/img/looper-3-1.png"
               alt="Looper"
             />
             <img
-              className="burst-pucker-3"
+              className={styles["burst-pucker-3"]}
               src="https://c.animaapp.com/8Gc7c0uK/img/burst-pucker-2-2.svg"
               alt="Burst Pucker 3"
             />
           </div>
-          <div class="rectangle"></div>
+          <div className={styles["rectangle"]}></div>
           <NavBar />
           <Outlet />
         </div>
