feae09378639e2a420e5b1fa81cb24c100cbeae7
chaewon02~ <glgl246@naver.com>
2024-09-16 17:31:50 +0900

    💄 FinancialStatements css 모듈화

--- investalk-front/src/components/detail/financialstatements/FinancialStatements.js
+++ investalk-front/src/components/detail/financialstatements/FinancialStatements.js
@@ -1,29 +1,33 @@
-import "./FinancialStatements.css";
+import styles from "./FinancialStatements.module.css";
 
 const FinancialStatements = () => {
   return (
     <div>
-      <div className="group-wrapper">
-        <div className="group-2">
-          <div className="text-wrapper-2">재무제표</div>
-          <div className="frame">
+      <div className={styles["group-wrapper"]}>
+        <div className={styles["group-2"]}>
+          <div className={styles["text-wrapper-2"]}>재무제표</div>
+          <div className={styles.frame}>
             {[...Array(3)].map((_, index) => (
-              <div className="frame-wrapper" key={index}>
-                <div className="frame-2">
-                  <div className="frame-3">
-                    <div className="frame-4">
-                      <div className="text-wrapper-3">수익성</div>
-                      <div className="text-wrapper-4">·</div>
-                      <div className="text-wrapper-5">분기별</div>
+              <div className={styles["frame-wrapper"]} key={index}>
+                <div className={styles["frame-2"]}>
+                  <div className={styles["frame-3"]}>
+                    <div className={styles["frame-4"]}>
+                      <div className={styles["text-wrapper-3"]}>수익성</div>
+                      <div className={styles["text-wrapper-4"]}>·</div>
+                      <div className={styles["text-wrapper-5"]}>분기별</div>
                     </div>
-                    <div className="frame-5">
+                    <div className={styles["frame-5"]}>
                       {["현금유동성", "순수익", "EPS", "마진"].map(
                         (item, idx) => (
-                          <div className="frame-6" key={idx}>
-                            <div className="text-wrapper-6">{item}</div>
-                            <div className="frame-7">
-                              <div className="text-wrapper-7">+</div>
-                              <div className="text-wrapper-8">30%</div>
+                          <div className={styles["frame-6"]} key={idx}>
+                            <div className={styles["text-wrapper-6"]}>
+                              {item}
+                            </div>
+                            <div className={styles["frame-7"]}>
+                              <div className={styles["text-wrapper-7"]}>+</div>
+                              <div className={styles["text-wrapper-8"]}>
+                                30%
+                              </div>
                             </div>
                           </div>
                         )
