3ffa9c6b9eeb9ca2186908bfd478565b5eaaaf1d
chaewon02~ <glgl246@naver.com>
2024-09-16 18:14:56 +0900

    ✨ Detail Page 컴포넌트 merge

--- investalk-front/src/pages/detail/DetailPage.js
+++ investalk-front/src/pages/detail/DetailPage.js
@@ -1,11 +1,17 @@
 import StockInfoCard from "../../components/detail/stockinfo/StockInfoCard";
 import FinancialStatements from "../../components/detail/financialstatements/FinancialStatements";
+import Chatting from "../../components/detail/chatting/Chatting";
+import Recommend from "../../components/detail/recommend/Recommend";
+import Analyze from "../../components/detail/analyze/Analyze";
 
 const DetailPage = () => {
   return (
     <div>
       <FinancialStatements />
       <StockInfoCard />
+      <Recommend />
+      <Analyze />
+      <Chatting />
     </div>
   );
 };
