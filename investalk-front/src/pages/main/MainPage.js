0adeb482594754981f34e849b5e26fd17f22437b
GEONWOO HONG <h46241767@gmail.com>
2024-09-17 13:54:40 +0900

    ✨ MainPage 조립

--- investalk-front/src/pages/main/MainPage.js
+++ investalk-front/src/pages/main/MainPage.js
@@ -1,5 +1,15 @@
+import Chatting from '../../components/detail/chatting/Chatting';
+import NewsComponent from '../../components/mainpage/news/NewsComponent';
+import DynamicBarChart from '../../components/mainpage/dailygraph/GraphComponent.js';
+
 const MainPage = () => {
-  return <div className="div-wrapper">main</div>;
+  return (
+    <div >
+      <DynamicBarChart />
+      <NewsComponent />
+      <Chatting />
+    </div>
+  );
 };
 
 export default MainPage;
