8c9cd1a7782491b6b36756b349dca18fbbfceb05
Sang Min Park <minisstar@naver.com>
2024-09-21 05:13:30 +0900

    ✨ 추천 섹터 부분 get요청 구현

--- investalk-back/api/routes.py
+++ investalk-back/api/routes.py
@@ -1,7 +1,22 @@
-from flask import Blueprint, jsonify
+import sys
+import os
 
+# 현재 파일 위치에서 상위 디렉토리 경로를 sys.path에 추가
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from flask import Blueprint, jsonify
+from .detail.recommend.sectorRecommend import get_ticker_sector, get_similar_sectors
+
+# Blueprint 생성
 api_bp = Blueprint('api', __name__)
 
-@api_bp.route('/data', methods=['GET'])
-def get_data():
-    return jsonify({"message": "This is the API endpoint"})
+@api_bp.route('/recommend-sectors', methods=['GET'])
+def recommend_sectors():
+    nvidia_ticker = "NVDA"
+    nvidia_sector = get_ticker_sector(nvidia_ticker)
+    similar_sectors = get_similar_sectors(nvidia_sector)
+    
+    return jsonify({
+        'nvidia_sector': nvidia_sector,
+        'similar_sectors': similar_sectors
+    })
--- investalk-back/app.py
+++ investalk-back/app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# 프로젝트의 루트 디렉토리를 sys.path에 추가
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+# 기존 코드
 from flask import Flask
 from api.routes import api_bp
 from crawling import crawler
@@ -5,7 +12,7 @@ from LLM import llm_model
 
 app = Flask(__name__)
 
-# Register Blueprints
+# Blueprint 등록
 app.register_blueprint(api_bp, url_prefix='/api')
 
 @app.route('/')
@@ -14,4 +21,3 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    aaa
