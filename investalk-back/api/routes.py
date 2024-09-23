14f5885b0a2db4d145a2838aa694e757640bcdd9
GEONWOO HONG <h46241767@gmail.com>
2024-09-23 19:53:23 +0900

    ✨ mainpage의 그래프 컴포넌트 back 구현

--- investalk-back/api/routes.py
+++ investalk-back/api/routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint
+from api.mainpage.dailygraph.dailyGraph import get_daily_graph_data
 
 api_bp = Blueprint('api', __name__)
 
-@api_bp.route('/data', methods=['GET'])
-def get_data():
-    return jsonify({"message": "This is the API endpoint"})
+@api_bp.route('/etf-data', methods=['GET'])
+def get_etf_data():
+    return get_daily_graph_data()
--- investalk-back/app.py
+++ investalk-back/app.py
@@ -1,16 +1,8 @@
 from flask import Flask
-from api.routes import api_bp
-from crawling import crawler
-from LLM import llm_model
+from flask_cors import CORS
+from api import create_app
 
-app = Flask(__name__)
-
-# Register Blueprints
-app.register_blueprint(api_bp, url_prefix='/api')
-
-@app.route('/')
-def home():
-    return "Hello, Flask!"
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
