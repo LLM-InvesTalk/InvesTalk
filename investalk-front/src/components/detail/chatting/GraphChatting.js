5cff129e464e58be5fed9a97566d391a1bf0a3a5
Sang Min Park <minisstar@naver.com>
2024-09-25 17:27:10 +0900

    ✨ 그래프용 채팅 컴포넌트화

---
+++ investalk-front/src/components/detail/chatting/GraphChatting.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Chatting.module.css"; // 기존 CSS 모듈을 그대로 import
+
+const GraphChatting = () => {
+  return (
+    <div className={styles.overlap5}>
+      <div className={styles.group8}>
+        <div className={styles.frame17}>
+          <p className={styles.p}>
+            그래프용 메세지에요
+          </p>
+        </div>
+        <img className={styles.frame18} src="https://c.animaapp.com/8Gc7c0uK/img/frame-141.svg" alt="frame 141" />
+        <div className={styles.textWrapper16}>00:00</div>
+      </div>
+      <div className={styles.rectangle7}></div>
+      <div className={styles.rectangle8}></div>
+    </div>
+  );
+};
+
+export default GraphChatting;
