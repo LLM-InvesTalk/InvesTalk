d4d31bf1a21efa2e0a236c5937100aa84c949016
chaewon02~ <glgl246@naver.com>
2024-09-28 20:36:08 +0900

    ✨ 애널리스트 평점 reverse

--- investalk-back/api/detail/stockinfo/stockinfo.py
+++ investalk-back/api/detail/stockinfo/stockinfo.py
@@ -13,6 +13,15 @@ def get_stockInfo(ticker_symbol):
     # 애널리스트 평점
     analyst_rating = ticker.info.get('recommendationMean', 'N/A')  # 1.0: Strong Buy, 5.0: Strong Sell
 
+    if analyst_rating != 'N/A':
+        try:
+            analyst_rating = float(analyst_rating)
+            reversed_analyst_rating = 5.0 - (analyst_rating - 1.0)
+        except ValueError:
+            reversed_analyst_rating = 'N/A'
+    else:
+        reversed_analyst_rating = 'N/A'
+
     # 섹터 정보
     sector = ticker.info.get('sector', 'N/A')
 
@@ -20,6 +29,6 @@ def get_stockInfo(ticker_symbol):
     return {
         "name": company_name,
         "symbol": symbol,
-        "analyst_rating": analyst_rating,
+        "analyst_rating": reversed_analyst_rating,
         "sector": sector
     }
