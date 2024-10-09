a1cc4e6c981d63923d3e9de20113f7f6e8c665bf
Sang Min Park <minisstar@naver.com>
2024-10-10 01:20:23 +0900

    ✨ 가격 데이터 수집 방법 수정

--- investalk-back/그래프.py
+++ investalk-back/그래프.py
@@ -16,39 +16,33 @@ def print_recent_data():
     market_open = now.replace(hour=9, minute=30, second=0, microsecond=0)
     market_close = now.replace(hour=16, minute=0, second=0, microsecond=0)
 
-    # 어제의 종가부터 현재까지의 가격 정보를 가져오기 위해 1분 간격 데이터를 가져옵니다.
+    # 어제의 종가부터 현재까지의 가격 정보를 가져오기 위해 5분 간격 데이터를 가져옵니다.
     if market_open <= now <= market_close:
         start_date = (now - timedelta(days=1)).strftime('%Y-%m-%d')
-        data = nvidia.history(interval="1m", start=start_date)
+        data = nvidia.history(interval="5m", start=start_date)
         
         # 데이터가 충분한지 확인합니다.
         if len(data) < 1:
             print("최근 데이터를 가져올 수 없습니다. 데이터를 확인해주세요.")
             return
-
-        # 어제 종가 이후 5분 간격으로 데이터 필터링
-        filtered_data = data.iloc[::5]
         
         # 데이터를 출력합니다.
         print("어제 종가부터 현재까지 5분 간격의 NVIDIA 주식 데이터:")
-        print(filtered_data)
+        print(data)
     else:
         # 장이 열리지 않았을 때, 그제 종가부터 어제 장 종료까지의 데이터를 가져옵니다.
         start_date = (now - timedelta(days=2)).strftime('%Y-%m-%d')
         end_date = (now - timedelta(days=1)).strftime('%Y-%m-%d')
-        data = nvidia.history(interval="1m", start=start_date, end=end_date)
+        data = nvidia.history(interval="5m", start=start_date, end=end_date)
         
         # 데이터가 충분한지 확인합니다.
         if len(data) < 1:
             print("최근 데이터를 가져올 수 없습니다. 데이터를 확인해주세요.")
             return
-
-        # 그제 종가부터 어제 장 종료까지 5분 간격으로 데이터 필터링
-        filtered_data = data.iloc[::5]
         
         # 데이터를 출력합니다.
         print("그제 종가부터 어제 장 종료까지 5분 간격의 NVIDIA 주식 데이터:")
-        print(filtered_data)
-
+        print(data)
+        
 if __name__ == "__main__":
     print_recent_data()
