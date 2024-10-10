1783b7e9abada0e3fa919c592fad732e7c238bfd
Sang Min Park <minisstar@naver.com>
2024-10-10 16:38:58 +0900

    ✨ 1분전으로 모든 내용 수정

--- investalk-back/그래프.py
+++ investalk-back/그래프.py
@@ -18,16 +18,16 @@ def print_recent_data():
     
     market_open = now.replace(hour=9, minute=30, second=0, microsecond=0)
     market_close = now.replace(hour=16, minute=0, second=0, microsecond=0)
-    market_close_minus_5 = now.replace(hour=15, minute=59, second=0, microsecond=0)  # 5분 전 시간 설정
+    market_close_minus_1 = now.replace(hour=15, minute=59, second=0, microsecond=0)  # 1분 전 시간 설정
 
     # 어제의 날짜를 구합니다.
     today_date = now.strftime('%Y-%m-%d')
 
     # 장이 열려 있을 경우 (어제의 9시 30분 ~ 16시)
     if market_open <= now <= market_close:
-        # 어제의 15시 55분부터 장 종료까지 데이터를 가져옵니다.
-        start_date = market_close_minus_5
-        data = nvidia.history(interval="1m", start=start_date)
+        # 어제의 15시 59분부터 장 종료까지 데이터를 가져옵니다.
+        start_date = market_close_minus_1
+        data = nvidia.history(interval="1m", start=start_date, end=market_close)
 
         # 데이터가 충분한지 확인합니다.
         if len(data) < 1:
@@ -35,12 +35,12 @@ def print_recent_data():
             return
         
         # 데이터를 출력합니다.
-        print("어제 3시 55분부터 장 종료까지 1분 간격의 NVIDIA 주식 데이터:")
+        print("어제 3시 59분부터 장 종료까지 1분 간격의 NVIDIA 주식 데이터:")
         print(data)
     else:
         # 어제 장이 열리지 않았을 때, 그제 종가부터 어제 장 종료까지의 데이터를 가져옵니다.
-        start_date = market_close_minus_5.strftime('%Y-%m-%d %H:%M:%S')
-        end_date = market_close.strftime('%Y-%m-%d %H:%M:%S')
+        start_date = market_close_minus_1
+        end_date = market_close
         data = nvidia.history(interval="1m", start=start_date, end=end_date)
 
         # 데이터가 충분한지 확인합니다.
@@ -49,7 +49,7 @@ def print_recent_data():
             return
         
         # 데이터를 출력합니다.
-        print("그제 3시 55분부터 어제 장 종료까지 1분 간격의 NVIDIA 주식 데이터:")
+        print("그제 3시 59분부터 어제 장 종료까지 1분 간격의 NVIDIA 주식 데이터:")
         print(data)
 
 if __name__ == "__main__":
