2907485fcec969298a388fac4ede862aebe5c112
Sang Min Park <minisstar@naver.com>
2024-10-16 20:27:01 +0900

    ✨ 코스 에러 해결

--- investalk-back/app.py
+++ investalk-back/app.py
@@ -1,6 +1,7 @@
 import sys
 import os
 from dotenv import load_dotenv  # .env 파일을 불러오기 위해 추가
+from flask_cors import CORS  # CORS 추가
 
 # 프로젝트의 루트 디렉토리를 sys.path에 추가
 sys.path.append(os.path.abspath(os.path.dirname(__file__)))
@@ -17,6 +18,9 @@ load_dotenv()
 
 # Flask 앱 생성
 app = create_app()  # create_app 함수 호출하여 앱 생성
+
+# CORS 설정
+CORS(app)  # 모든 출처에 대해 CORS 허용
 
 # Blueprint 등록
 app.register_blueprint(api_bp, url_prefix='/api')  # api 관련 Blueprint 등록
