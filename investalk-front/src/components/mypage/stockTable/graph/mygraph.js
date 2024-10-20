48687fe4016e36ba0796e816260e1d924c891a85
Sang Min Park <minisstar@naver.com>
2024-10-20 21:45:46 +0900

    ✨데이터에 의한 그래프 반영 완료

--- investalk-front/src/components/mypage/stockTable/graph/mygraph.js
+++ investalk-front/src/components/mypage/stockTable/graph/mygraph.js
@@ -1,17 +1,30 @@
-import { AreaPlot, lineElementClasses } from "@mui/x-charts/LineChart";
-import { areaElementClasses } from "@mui/x-charts/LineChart";
-import { useYScale, useDrawingArea } from "@mui/x-charts/hooks";
-import { LinePlot } from "@mui/x-charts/LineChart";
+import React from "react";
+import {
+  AreaPlot,
+  LinePlot,
+  lineElementClasses,
+  areaElementClasses,
+} from "@mui/x-charts/LineChart";
 import { ResponsiveChartContainer } from "@mui/x-charts/ResponsiveChartContainer";
 import { ChartsReferenceLine } from "@mui/x-charts/ChartsReferenceLine";
+import { useYScale, useDrawingArea } from "@mui/x-charts/hooks";
 
 const MyGraph = ({ data }) => {
-  // 그래프 데이터의 y값을 기반으로 x값을 자동으로 생성합니다.
+  // x값 생성
   const dataset = data.map((y, index) => ({ x: index, y }));
+
+  // 데이터의 평균값 계산
+  const yMean = data.reduce((a, b) => a + b, 0) / data.length;
+
+  // y축 범위를 평균값을 중심으로 좁게 설정 (예: ±0.5)
+  const yRange = 2;
+  const adjustedYMin = yMean - yRange;
+  const adjustedYMax = yMean + yRange;
 
   const config = {
     dataset: dataset,
-    xAxis: [{ dataKey: "x" }],
+    xAxis: [{ dataKey: "x", hide: true }], // x축 숨김 처리
+    yAxis: [{ min: adjustedYMin, max: adjustedYMax, hide: true }], // y축 숨김 처리
     series: [
       {
         type: "line",
@@ -24,7 +37,7 @@ const MyGraph = ({ data }) => {
     ],
     width: 50,
     height: 50,
-    margin: { top: 0, bottom: 30, left: 0, right: 0 },
+    margin: { top: 0, bottom: 0, left: 0, right: 0 },
     sx: {
       [`& .${areaElementClasses.root}`]: {
         fill: "url(#swich-color-id-1)",
@@ -64,11 +77,12 @@ const MyGraph = ({ data }) => {
     <ResponsiveChartContainer {...config}>
       <LinePlot />
       <AreaPlot />
-      <ChartsReferenceLine y={0} />
+      {/* 필요에 따라 레퍼런스 라인 제거 */}
+      {/* <ChartsReferenceLine y={0} /> */}
       <ColorSwich
         color1="rgb(225, 247, 255)"
         color2="rgb(247, 239, 255)"
-        threshold={0}
+        threshold={yMean}
         id="swich-color-id-1"
       />
     </ResponsiveChartContainer>
