02ff4976101f3520692794f0f0669d1e7abbc461
chaewon02~ <glgl246@naver.com>
2024-10-20 19:35:19 +0900

    ✨ 채팅 input 컴포넌트 css 분리 및 수정

--- investalk-front/src/components/detail/chatting/ChatInput.js
+++ investalk-front/src/components/detail/chatting/ChatInput.js
@@ -1,13 +1,20 @@
 import React from "react";
-import styles from "./Chatting.module.css"; // 기존 CSS 모듈을 그대로 import
+import styles from "./ChatInput.module.css"; // 기존 CSS 모듈을 그대로 import
 
-const ChatInput = () => {
+const ChatInput = ({ scrollToTop, showScrollToTop }) => {
   return (
     <div className={styles.group10}>
       <div className={styles.overlap6}>
         <div className={styles.textWrapper19}>채팅채팅채팅채팅</div>
       </div>
-      <img className={styles.vector2} src="https://c.animaapp.com/8Gc7c0uK/img/vector-1.svg" alt="vector" />
+      {showScrollToTop && (
+        <img
+          className={styles.vector2}
+          src="https://c.animaapp.com/8Gc7c0uK/img/vector-1.svg"
+          alt="vector"
+          onClick={() => scrollToTop()}
+        />
+      )}
     </div>
   );
 };
