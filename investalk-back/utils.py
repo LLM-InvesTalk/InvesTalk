7ac004e8daa270b34843e3ed68181606b5fb188c
GEONWOO HONG <h46241767@gmail.com>
2024-11-26 15:49:38 +0900

    ⚡️ current_user를 매개변수로 전달하도록 데코레이터 수정

--- investalk-back/utils.py
+++ investalk-back/utils.py
@@ -1,26 +1,28 @@
 # utils.py
+from functools import wraps
+from flask import request, jsonify
 import jwt
 import os
-from flask import request, jsonify
-from functools import wraps
 
-# 환경 변수에서 SECRET_KEY 가져오기
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # JWT 검증 데코레이터
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        token = request.cookies.get('jwt')  # 쿠키에서 토큰 가져오기
+        token = request.cookies.get('jwt')
         if not token:
-            return jsonify({"error": "Unauthorized, please log in"}), 401  # 인증 실패 시
+            return jsonify({"error": "Unauthorized, please log in"}), 401
 
         try:
-            jwt.decode(token, SECRET_KEY, algorithms=["HS256"])  # JWT 토큰 검증
+            data = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            current_user = data
+            print(f"Decoded JWT: {current_user}")  # 디버깅 로그
         except jwt.ExpiredSignatureError:
             return jsonify({"error": "Token has expired"}), 401
         except jwt.InvalidTokenError:
             return jsonify({"error": "Invalid token"}), 401
 
-        return f(*args, **kwargs)  # JWT가 유효하면 원래 함수 실행
+        return f(current_user=current_user, *args, **kwargs)
     return decorated
+
