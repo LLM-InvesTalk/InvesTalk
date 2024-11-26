84a3e2bf1a4b1e5c33b66856587f00f1b15ef14e
GEONWOO HONG <h46241767@gmail.com>
2024-11-26 15:51:12 +0900

    ⚡️ OPTIONS 요청 전역처리를 위한 app.py 수정

--- investalk-back/app.py
+++ investalk-back/app.py
@@ -19,8 +19,19 @@ load_dotenv()
 app = create_app()
 
 # CORS 설정
-CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:3000"}})
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "http://localhost:3000"}}, methods=["GET","POST","DELETE","PUT","OPTIONS"])
 
+# OPTIONS 요청 전역 처리
+@app.before_request
+def handle_options_request():
+    if request.method == "OPTIONS":
+        response = jsonify({"message": "OK"})
+        response.headers.add("Access-Control-Allow-Origin", "http://localhost:3000")
+        response.headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        response.headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+        response.headers.add("Access-Control-Allow-Credentials", "true")
+        return response, 200
+        
 # Blueprint 등록
 app.register_blueprint(api_bp, url_prefix='/api')  # api 관련 Blueprint 등록
 app.register_blueprint(login_bp, url_prefix='/login')  # login 관련 Blueprint 등록
