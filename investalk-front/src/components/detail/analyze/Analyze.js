3c2cdacd5938ba2d76a5eccce9c1ec6d7da16076
chaewon02 <glgl246@naver.com>
2025-01-18 19:18:18 +0900

    ✨ 특정 종목에 대한 분석 프론트 api 연결

--- investalk-front/src/components/detail/analyze/Analyze.js
+++ investalk-front/src/components/detail/analyze/Analyze.js
@@ -1,19 +1,42 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import styles from "./Analyze.module.css"; // CSS 모듈 import
+import axios from "axios";
 
-const Analyze = () => {
+const Analyze = (props) => {
+  const { tickerSymbol } = props;
+
+  const [assistockAnalyze, setAssistockAnalyze] = useState({});
+  const [loading, setLoading] = useState(false); // 로딩 상태 관리
+
+  useEffect(() => {
+    const getAnalyze = async () => {
+      try {
+        setLoading(true); // 데이터 요청 시작 시 로딩 true
+        const response = await axios.get(
+          `http://localhost:5000/api/chat/analyze/${tickerSymbol}`
+        );
+        console.log("fetch data: ", response.data);
+  
+        setAssistockAnalyze(response.data);
+      } catch (error) {
+        console.error("Error fetching scraps:", error);
+      } finally {
+        setLoading(false); // 완료 후 로딩 false
+      }
+    }
+    getAnalyze();
+  }, [tickerSymbol]);
+
   return (
     <div className={styles.divWrapper}>
       <div className={styles.overlapWrapper}>
         <div className={styles.overlap3}>
           <div className={styles.textWrapper}>Assistock 분석서</div>
           <div className={styles.textDescription}>
-            Text Description...<br />
-            .................................................................<br />
-            .................................................................<br />
-            ...............................................................<br />
-            .........................................<br />
-            .............................................................
+          {loading ? (
+              <div>Loading...</div>
+            ) :( assistockAnalyze.reply ? assistockAnalyze.reply : "데이터가 없습니다.")}
           </div>
         </div>
       </div>
